9071d771f92298bf9d676783d7d128284ffc9d27
kurama8103 <38579582+kurama8103@users.noreply.github.com>
2023-08-12 17:53:16 +0900

    add

--- remove_by_rembg.py
+++ remove_by_rembg.py
@@ -2,20 +2,19 @@
 import sys
 from PIL import Image
 from rembg import remove
+from resize_img import resize_img
 
 
-def remove_by_rembg(filepath: str, max_size: float = 500):
-    img = Image.open(filepath)
-    img.resize([max_size*i//max(img.width, img.height)
-                for i in [img.width, img.height]])
+def remove_by_rembg(filepath: str):
+    img = resize_img(filepath)
     return remove(img)
 
 
 if __name__ == '__main__':
     if 'ipykernel_launcher.py' in sys.argv[0]:
-        filepath = './data/test.jpg'
+        filepath = '../data/test.jpg'
     else:
         filepath = sys.argv[1]
-    remove_by_rembg(filepath, 300).show()
+    remove_by_rembg(filepath).show()
 
 # %%
