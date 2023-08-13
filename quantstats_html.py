9ec71f33708d63d04c7ac773ab54114287cf7529
kurama8103 <38579582+kurama8103@users.noreply.github.com>
2023-08-13 17:46:17 +0900

    revise

--- quantstats_html.py
+++ quantstats_html.py
@@ -1,24 +1,40 @@
 # %%
 import subprocess
 import sys
-import quantstats as qs
+import warnings
+
 import pandas as pd
+import quantstats as qs
+from tqdm.auto import tqdm
+
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 
-def quantstats_html(filepath_csv, bm=None):
+def quantstats_html(filepath_csv: str, open_html: bool = True) -> None:
+    """
+    first column is used benchmark. 
+    """
     qs.extend_pandas()
     df = pd.read_csv(filepath_csv, index_col=0,
-                     parse_dates=True, usecols=[0, 1]).iloc[:, 0]
-    if min(df) <= 0:
+                     parse_dates=True)
+    if df.min().min() <= 0:
         pass
     else:
         df = df.pct_change().dropna()
-    filename_html = df.name+'-' + \
-        df.index[0].strftime('%Y%m%d')+'-' + \
-        df.index[-1].strftime('%Y%m%d')+'.html'
-    qs.reports.html(df, bm=bm, rf=0., title=df.name,
-                    download_filename=filename_html, output='.')
-    return filename_html
+
+    for c in tqdm(df.columns):
+        x = df[c]
+        if df.iloc[:, 0].name == c:
+            bm = None
+        else:
+            bm = df.iloc[:, 0]
+        fn = 'qs_'+x.name+x.index[-1].strftime('_%Y%m%d')+'.html'
+        print('', c)
+        qs.reports.html(returns=x, rf=0., title=c, benchmark=bm,
+                        download_filename=fn, output='./')
+        if open_html == True:
+            subprocess.call(['open', fn])
+    return None
 
 
 if __name__ == '__main__':
@@ -26,7 +42,6 @@ if __name__ == '__main__':
         filepath_csv = '../data/test.csv'
     else:
         filepath_csv = sys.argv[1]
-    filename_html = quantstats_html(filepath_csv)
-    subprocess.call(['open', filename_html])
+    quantstats_html(filepath_csv)
 
 # %%
